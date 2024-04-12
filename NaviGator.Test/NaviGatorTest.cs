10e701bfb83a24c3b5e17afb27bbed32bb74c9e8
Roberto Ravale <robertoravale97@gmail.com>
2024-04-13 00:25:55 +0200

    Test UC7

--- NaviGator.Test/NaviGatorTest.cs
+++ NaviGator.Test/NaviGatorTest.cs
@@ -61,7 +61,7 @@ namespace NaviGator.Test
             naviGator.CaricaCabine();
             naviGator.RegistraCabina("1");
             naviGator.RegistraCliente("Pippo", "Franco", "AQ12SD34DE12", "AZ1234567", "3324343434", "4050987845464049");
-            naviGator.RegistraPrenotazione(DateTime.Parse("2022-03-11"), DateTime.Parse("2022-03-14"));
+            naviGator.RegistraPrenotazione(DateTime.Parse("2024-03-11"), DateTime.Parse("2024-03-18"));
 
             //Act
             var prenotazioneInCorso = naviGator.GetPrenotazioneInCorso();
@@ -69,6 +69,64 @@ namespace NaviGator.Test
             //Assert
             Assert.NotNull(prenotazioneInCorso);
         }
+
+//**************TEST UC7**************
+        [Fact]
+        public void TestCreaNuovoServizioInCabina()
+        {
+            //Assign
+            naviGator.CaricaCabine();
+            naviGator.RegistraCabina("1");
+            naviGator.RegistraCliente("Pippo", "Franco", "AQ12SD34DE12", "AZ1234567", "3324343434", "4050987845464049");
+            naviGator.RegistraPrenotazione(DateTime.Parse("2024-03-11"), DateTime.Parse("2024-03-18"));
+        
+            //Act
+            naviGator.CreaServizioCabina("1", DateTime.Parse("2022-03-30"));
+        
+            //Assert
+            Assert.NotNull(naviGator.GetPrenotazioneInCorso());
+            Assert.NotNull(naviGator.GetServizioCabinaInCorso());
+        }
+        
+        [Fact]
+        public void TestRegistraPortate()
+        {
+            //Assign
+            naviGator.CaricaCabine();
+            naviGator.RegistraCabina("1");
+            naviGator.RegistraCliente("Pippo", "Franco", "AQ12SD34DE12", "AZ1234567", "3324343434", "4050987845464049");
+            naviGator.RegistraPrenotazione(DateTime.Parse("2024-03-11"), DateTime.Parse("2024-03-18"));
+            naviGator.CreaServizioCabina("1", DateTime.Parse("2022-03-30"));
+            Portata portata = new Portata("Tiramisu", true, 1, "01");
+        
+            //Act
+            naviGator.GetServizioCabinaInCorso().RegistraPortata(portata, 1);
+        
+            //Assert
+            Assert.NotNull(naviGator.GetPrenotazioneInCorso());
+            Assert.NotNull(naviGator.GetServizioCabinaInCorso());
+        }
+        
+        [Fact]
+        public void TestRegistraServizioInCamera()
+        {
+            //Assign
+            naviGator.CaricaCabine();
+            naviGator.RegistraCabina("1");
+            naviGator.RegistraCliente("Pippo", "Franco", "AQ12SD34DE12", "AZ1234567", "3324343434", "4050987845464049");
+            naviGator.RegistraPrenotazione(DateTime.Parse("2024-03-11"), DateTime.Parse("2024-03-18"));
+            naviGator.CreaServizioCabina("1", DateTime.Parse("2022-03-30"));
+            Portata portata = new Portata("Tiramisu", true, 1, "01");
+            naviGator.GetServizioCabinaInCorso().RegistraPortata(portata, 1);
+        
+            //Act
+            naviGator.RegistraServizioCabina();
+        
+            //Assert
+            Assert.NotNull(naviGator.GetPrenotazioneInCorso());
+            Assert.NotNull(naviGator.GetServizioCabinaInCorso());
+        }
+
         
     }
 }
